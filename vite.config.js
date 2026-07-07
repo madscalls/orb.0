fba34e4d5e8d7c4c26b0851ae92cb30ad0ea6f64
Madss Calls <madscalls@gmail.com>
2026-07-06 21:35:06 -0400

    Fix module bundling and add favicon

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/orb.0/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
